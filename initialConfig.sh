8da1680242022e8ef3ebd46509e90886c3e0bb26
Daniel Machin <jmdanny@gmail.com>
2016-04-22 13:52:04 -0400

    Disable Safari "safe" file auto open. Added dividers for easier read.

--- initialConfig.sh
+++ initialConfig.sh
@@ -1,21 +1,36 @@
 #!/bin/bash
 
-##  DON'T CREATE DS_STORE FILES ON NETWORK
-defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+##----------------SECURITY---------------------
 
 ## SET SCREENSAVER TO PROMPT IMMEDIATELY FOR PASSWORD
 defaults write com.apple.screensaver askForPassword -int 1
 defaults write com.apple.screensaver askForPasswordDelay -int 0
 
-## Set Documents as the default location for new Finder windows
-defaults write com.apple.finder NewWindowTarget -string "PfLo"
-defaults write com.apple.finder NewWindowTargetPath -string "file://~/Documents/"
+##-----------------SYSTEM----------------------
+
+## HIDE USER LIBRARY (JUST IN CASE WE CHANGED IT BEFORE)
+chflags hidden ~/Library
+
+##--------------USER SETTINGS-----------------
 
 ## TURN OFF FAST USER SWITCHING
 defaults write /Library/Preferences/.GlobalPreferences MultipleSessionEnabled -bool false
 
-## HIDE USER LIBRARY (JUST IN CASE WE CHANGED IT BEFORE)
-chflags hidden ~/Library
+##-----------------FINDER----------------------
+
+## Set Documents as the default location for new Finder windows
+defaults write com.apple.finder NewWindowTarget -string "PfLo"
+defaults write com.apple.finder NewWindowTargetPath -string "file://~/Documents/"
+
+##  DON'T CREATE DS_STORE FILES ON NETWORK
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
+##-----------------SAFARI----------------------
+
+## DISABLE SAFARI AUTO OPEN FOR "SAFE" FILES
+defaults write com.apple.Safari AutoOpenSafeDownloads -bool false
+
+##-----------------ACTIONS--------------------
 
 ## RUN ALL MACOS UPDATES
 softwareupdate --install -all
